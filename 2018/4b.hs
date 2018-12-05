b57751d8c39f50a0988405f774538f4c77c9fa51
Ryan Norris <rnorris@palantir.com>
2018-12-05 12:29:37 +0900

    Pull out common day 4 code

--- 2018/4b.hs
+++ 2018/4b.hs
@@ -1,65 +1,9 @@
 import Data.List 
 import Data.List.Split
 
-data Timestamp = Timestamp Int Int Int Int Int deriving (Show, Eq, Ord)
-data Action = Begin Int | Sleep | Wake deriving (Show, Eq, Ord)
-data Event = Event Timestamp Action deriving (Show, Eq, Ord)
-data Shift = Shift Int [Event] deriving (Show, Eq, Ord)
-data SleepMinute = SleepMinute Int Int deriving(Show, Eq, Ord)
+import Day4
 
-shiftGuard :: Shift -> Int
-shiftGuard (Shift id _) = id
-
-minuteGuard :: SleepMinute -> Int
-minuteGuard (SleepMinute id _) = id
-
-minuteMinute :: SleepMinute -> Int
-minuteMinute (SleepMinute _ m) = m
-
-isBegin :: Event -> Bool
-isBegin (Event _ (Begin _)) = True
-isBegin _ = False
-
-replace :: String -> Char -> String -> String
-replace chars target s = map (\c -> if (elem c chars) then target else c) s
-
-remove :: String -> String -> String
-remove chars s = filter (\c -> not $ elem c chars) s
-
-constructTimestamp :: [Int] -> Timestamp
-constructTimestamp xs = Timestamp (xs!!0) (xs!!1) (xs!!2) (xs!!3) (xs!!4)
-
-constructAction :: [String] -> Action
-constructAction ["falls", _] = Sleep
-constructAction ["wakes", _] = Wake
-constructAction xs = Begin (read $ remove "#" (xs!!1))
-
-constructShift :: [Event] -> Shift
-constructShift ((Event _ (Begin x)):es) = Shift x es
-constructShift _ = Shift 0 []
-
-constructShifts :: [Event] -> [Shift]
-constructShifts events = map constructShift $ (split $ keepDelimsL $ whenElt isBegin) events
-
-sleepMinutes :: Int -> Event -> Event -> [SleepMinute]
-sleepMinutes id (Event (Timestamp _ _ _ _ m1) Sleep) (Event (Timestamp _ _ _ _ m2) Wake) = [SleepMinute id m | m <- [m1..(m2-1)]]
-sleepMinutes _ _ _ = []
-
-shiftSleepMinutes :: Shift -> [SleepMinute]
-shiftSleepMinutes (Shift id es) = foldl (++) [] $ map (uncurry (sleepMinutes id)) $ zip es (drop 1 es)
-
-parseLine s = Event timestamp action where
-    timestamp = constructTimestamp $ map read $ take 5 split
-    action = constructAction $ drop 5 split
-    split = words $ replace "[]-:" ' ' s
-
-parse :: String -> [Event]
-parse = (map parseLine) . lines
-
-swap :: (Int, Int) -> (Int, Int)
-swap (a, b) = (b, a)
-
-solve events = sortBy (\l1 l2 -> compare (length l2) (length l1)) $ groupBy (==) $ sort $ sleepMinutes where
+solve events = head $ head $ sortBy (\l1 l2 -> compare (length l2) (length l1)) $ groupBy (==) $ sort $ sleepMinutes where
     sleepMinutes = foldl (++) [] $ map shiftSleepMinutes $ constructShifts $ sort $ events
 
 main :: IO()
