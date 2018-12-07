1f80d332f9e23e05cef6f74b92e449275793fdc2
Ryan Norris <rnorris@palantir.com>
2018-12-07 15:29:41 +0900

    Tidy up 7b code

--- 2018/7b.hs
+++ 2018/7b.hs
@@ -4,9 +4,8 @@ import Data.Char
 import Data.List 
 import Data.Maybe
 
-import Debug.Trace
-
 data Task = Task Step Int deriving (Show, Eq, Ord)
+data Workshop = Workshop { workers :: Int, conds :: [Condition], steps :: [Step], tasks :: [Task] } deriving (Show)
 
 fromStep :: Step -> Task
 fromStep s = Task s (duration s)
@@ -23,19 +22,30 @@ isDone (Task _ t) = t == 0
 duration :: Step -> Int
 duration s = (ord s) - (ord 'A') + 61
 
-totalDuration :: Int -> [Condition] -> [Step] -> [Task] -> Int
-totalDuration w cs ss ts
-    | null ts && null ss = 0
-    | length finished > 0 = totalDuration w (filter (\c -> not $ elem (condDep c) finished) cs) ss (filter (not . isDone) ts)
-    | length ts < w && isJust next = let next = nextToRun cs ss in
-        totalDuration w cs (filter (/= (fromJust next)) ss) ((fromStep $ (fromJust next)) : ts)
-    | otherwise = 1 + (totalDuration w cs ss ((\ts -> traceShow ts ts) $ map tick ts))
-    where
-        finished = map toStep $ filter isDone ts
-        next = nextToRun cs ss
+workIsDone :: Workshop -> Bool
+workIsDone (Workshop _ _ ss ts) = null ts && null ss
+
+assignTasks :: Workshop -> Workshop
+assignTasks (Workshop w cs ss ts)
+    | isJust next = let s = fromJust next in assignTasks $ Workshop w cs (filter (/=s) ss) (fromStep s : ts)
+    | otherwise = Workshop w cs ss ts
+    where next = nextToRun cs ss
+
+finishTasks :: Workshop -> Workshop
+finishTasks (Workshop w cs ss ts) = Workshop w (filter (\c -> not $ elem (condDep c) finished) cs) ss running
+    where running = filter (not . isDone) ts
+          finished = map toStep $ filter isDone ts
+
+doWork :: Workshop -> Workshop
+doWork (Workshop w cs ss ts) = Workshop w cs ss (map tick ts)
+
+totalDuration :: Workshop -> Int
+totalDuration ws
+    | workIsDone ws = 0
+    | otherwise = 1 + (totalDuration $ finishTasks $ doWork $ assignTasks ws)
 
 solve :: [Condition] -> Int
-solve cs = totalDuration 5 cs (allSteps cs) []
+solve cs = totalDuration (Workshop 5 cs (allSteps cs) [])
 
 main :: IO()
 main = interact (show . solve . parse)
