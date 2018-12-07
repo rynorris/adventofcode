3df337d9f3e37f81bf0a3f9fb31f83afdb949810
Ryan Norris <rnorris@palantir.com>
2018-12-07 15:01:56 +0900

    Refactor common code

--- 2018/7b.hs
+++ 2018/7b.hs
@@ -1,37 +1,10 @@
+import Day7
+
 import Data.Char
 import Data.List 
 import Data.Maybe
 
 import Debug.Trace
-
-type Step = Char
-data Condition = Condition Step Step deriving (Show, Eq, Ord)
-
-condDep :: Condition -> Step
-condDep (Condition d _) = d
-
-condStep :: Condition -> Step
-condStep (Condition _ s) = s
-
-parseCondition :: String -> Condition
-parseCondition line = Condition (head (ws !! 1)) (head (ws !! 7)) where
-    ws = words line
-
-parse :: String -> [Condition]
-parse = map parseCondition . lines
-
-allSteps :: [Condition] -> [Step]
-allSteps cs = nub $ sort $ (map condDep cs) ++ (map condStep cs)
-
-canRun :: [Condition] -> Step -> Bool
-canRun cs s = null $ filter (== s) $ map condStep cs
-
-nextToRun :: [Condition] -> [Step] -> Maybe Step
-nextToRun cs ss | null runnable  = Nothing
-                | otherwise = Just (head runnable)
-    where runnable = filter (canRun cs) ss
-
--- NEW
 
 data Task = Task Step Int deriving (Show, Eq, Ord)
 
---
+++ 2018/Day7.hs
@@ -0,0 +1,29 @@
+module Day7 where
+
+type Step = Char
+data Condition = Condition Step Step deriving (Show, Eq, Ord)
+
+condDep :: Condition -> Step
+condDep (Condition d _) = d
+
+condStep :: Condition -> Step
+condStep (Condition _ s) = s
+
+parseCondition :: String -> Condition
+parseCondition line = Condition (head (ws !! 1)) (head (ws !! 7)) where
+    ws = words line
+
+parse :: String -> [Condition]
+parse = map parseCondition . lines
+
+allSteps :: [Condition] -> [Step]
+allSteps cs = nub $ sort $ (map condDep cs) ++ (map condStep cs)
+
+canRun :: [Condition] -> Step -> Bool
+canRun cs s = null $ filter (== s) $ map condStep cs
+
+nextToRun :: [Condition] -> [Step] -> Maybe Step
+nextToRun cs ss | null runnable  = Nothing
+                | otherwise = Just (head runnable)
+    where runnable = filter (canRun cs) ss
+
