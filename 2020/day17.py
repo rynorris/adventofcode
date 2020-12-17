a01b62c1c12900ce5de0487fc657ee297ce86db9
Ryan Norris <rnorris@palantir.com>
2020-12-17 15:00:19 +0900

    Refactor day 17 to be more DRY

--- 2020/day17.py
+++ 2020/day17.py
@@ -3,31 +3,64 @@ import itertools
 
 inp = get_input(2020, 17)
 
-state = {}
-for x, row in enumerate(inp.split("\n")):
-    for y, c in enumerate(row):
-        state[(x, y, 0, 0)] = c
+
+def initial(inp, dim):
+    state = {}
+    for y, row in enumerate(inp.split("\n")):
+        for x, c in enumerate(row):
+            coord = [0] * dim
+            coord[0] = x
+            coord[1] = y
+            state[tuple(coord)] = c
+    return state
 
 
-def neighbours(x, y, z, w):
-    return [(x+dx, y+dy, z+dz, w+dw) for dx, dy, dz, dw in itertools.product([-1, 0,1],[-1, 0,1],[-1, 0,1], [-1,0,1]) if not dx == dy == dz == dw == 0]
+def mask_slice(state, mask):
+    def matches(coord):
+        return all([y is None or x == y for x, y in zip(coord, mask)])
+
+    def project(coord):
+        return tuple([c for c, m in zip(coord, mask) if m is None])
+
+    return {project(c): v for c, v in state.items() if matches(c)}
+
+
+def add_tuples(t, s):
+    return tuple([a + b for a, b in zip(t, s)])
+
+
+def neighbours(c):
+    return [add_tuples(c, dc) for dc in itertools.product(*([[-1,0,1]] * len(c))) if not dc == tuple([0] * len(c))]
 
 
 def step(state):
     new = {}
 
-    allcoords = set([c for x, y, z, w in state.keys() for c in neighbours(x, y, z, w)])
-    for x, y, z, w in allcoords:
-        c = state.get((x, y, z, w), ".")
-        num_active = len([1 for c in neighbours(x, y, z, w) if state.get(c, ".") == "#"])
-        if c == "#" and num_active in [2, 3]:
-            new[(x, y, z, w)] = "#"
-        elif c == "." and num_active == 3:
-            new[(x, y, z, w)] = "#"
+    allcoords = set([n for c in state.keys() for n in neighbours(c)])
+    for c in allcoords:
+        s = state.get(c, ".")
+        num_active = len([1 for c in neighbours(c) if state.get(c, ".") == "#"])
+        if s == "#" and num_active in [2, 3]:
+            new[c] = "#"
+        elif s == "." and num_active == 3:
+            new[c] = "#"
 
     return new
 
-for ix in range(6):
-    state = step(state)
+def part_a():
+    state = initial(inp, 3)
+
+    for ix in range(6):
+        state = step(state)
     print(len([c for c in state.values() if c == "#"]))
+
+def part_b():
+    state = initial(inp, 4)
+
+    for ix in range(6):
+        state = step(state)
+    print(len([c for c in state.values() if c == "#"]))
+
+part_a()
+part_b()
 
